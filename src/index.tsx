6f06deb4ef4a98eff3e9d46a85929520d01047fa
hamzicabdulah <abdulahhamzic@gmail.com>
2017-12-20 21:54:34 +0100

    Removed redundant variable

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-const initialState: any = undefined;
-
 ReactDOM.render(<App />, document.getElementById('root'));
